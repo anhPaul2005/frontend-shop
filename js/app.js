0edf84e22264ce7d4459d998135cf2f447a8c288
anhPaul2005 <tanh29012004@gmail.com>
2025-12-17 01:46:40 +0700

    fix v6

--- js/app.js
+++ js/app.js
@@ -1,13 +1,8 @@
 const API_URL = "https://backend-api-1-t9qm.onrender.com/products";
 
-// test log
-console.log("Fetching products...");
-
 fetch(API_URL)
   .then(res => res.json())
   .then(data => {
-    console.log("DATA:", data);
-
     const list = document.getElementById("product-list");
     list.innerHTML = "";
 
@@ -21,8 +16,9 @@ fetch(API_URL)
         <div class="col-sm-6 col-md-4 col-lg-3">
           <div class="product-card">
             <img 
-              src="https://picsum.photos/300/200?random=${i}" 
+              src="https://picsum.photos/400/300?random=${i}" 
               class="product-img"
+              alt="${p.name}"
             >
             <div class="product-body">
               <div class="product-name">${p.name}</div>
@@ -36,8 +32,7 @@ fetch(API_URL)
       `;
     });
   })
-  .catch(err => {
-    console.error("FETCH ERROR:", err);
+  .catch(() => {
     document.getElementById("product-list").innerHTML =
       "<p class='text-danger text-center'>Không kết nối được backend</p>";
   });
