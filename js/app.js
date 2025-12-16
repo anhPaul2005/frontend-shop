a9807f6eed9d6398d0a9ece463989f3c8a110092
anhPaul2005 <tanh29012004@gmail.com>
2025-12-17 01:38:24 +0700

    fix v3

--- js/app.js
+++ js/app.js
@@ -4,21 +4,30 @@ fetch(API_URL)
   .then(res => res.json())
   .then(data => {
     const list = document.getElementById("product-list");
+
+    if (!data || data.length === 0) {
+      list.innerHTML = "<p class='text-center'>Không có sản phẩm</p>";
+      return;
+    }
+
     list.innerHTML = "";
 
     data.forEach(p => {
       list.innerHTML += `
-        <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+        <div class="col-sm-6 col-md-4 col-lg-3">
           <div class="product-card">
             <div class="product-name">${p.name}</div>
-            <div class="product-price">${Number(p.price).toLocaleString()} ₫</div>
-            <button class="btn btn-buy">Mua ngay</button>
+            <div class="product-price">
+              ${Number(p.price).toLocaleString()} ₫
+            </div>
+            <button class="btn btn-buy w-100">Mua ngay</button>
           </div>
         </div>
       `;
     });
   })
-  .catch(() => {
+  .catch(err => {
     document.getElementById("product-list").innerHTML =
-      "<p class='text-center text-danger'>Không tải được dữ liệu</p>";
+      "<p class='text-danger text-center'>Lỗi tải dữ liệu</p>";
+    console.error(err);
   });
